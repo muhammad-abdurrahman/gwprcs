b94f80c63315036e5790e6950dd21db6558af39f
Muhammad Abdurrahman <peace_n_blessings@live.com>
2015-03-08 14:05:30 +0000

    -> Migrated project from sts 3.1.0 to 3.1.6.3 -> Fixed problem with dependency problem by doing the following:       1 - Open the project's properties (e.g., right-click on the project's name in the project explorer and select "Properties").       2 - Select "Deployment Assembly".       3 - Click the "Add..." button on the right margin.       4 - Select "Java Build Path Entries" from the menu of Directive Type and click "Next".       5 - Select "Maven Dependencies" from the Java Build Path Entries menu and click "Finish".

--- src/main/java/org/gwizards/prcs/controller/DocumentController.java
+++ src/main/java/org/gwizards/prcs/controller/DocumentController.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import org.gwizards.prcs.LiveProject;
 import org.gwizards.prcs.LiveProjectDocument;
+import org.gwizards.prcs.LiveProjectStage;
+import org.gwizards.prcs.LiveProjectStageDocument;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,23 +16,44 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping(value = "/document")
 public class DocumentController {
-	@RequestMapping(value = "/main", method = RequestMethod.GET)
-	public String showDocumentMain(Model model) {
-		List<LiveProject> liveProjects = LiveProject.findAllLiveProjects();
-		model.addAttribute("projects", liveProjects);
-		return "document-main";
-	}
-
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/project", method = RequestMethod.GET)
 	public String showDocumentsForProject(@RequestParam(value = "pid") int pid,
 			Model model) {
 		LiveProject liveProject = LiveProject.findLiveProject(pid);
+
+		liveProject.getLiveProjectStages();
+		// Set<LiveProjectStage> lpses =
+		// LiveProject.findLiveProject(1).getLiveProjectStages();
+		// for (LiveProjectStage lps : lpses){
+		// lps.getStageNo().
+		// }
+
+		// LiveProjectStage lps = LiveProjectStage.findLiveProjectStage(1);
+		// lps.getStageNo().getStageNo();
+
 		Set<LiveProjectDocument> liveProjectDouments = liveProject
 				.getLiveProjectDocuments();
 		List<LiveProject> liveProjects = LiveProject.findAllLiveProjects();
+
 		model.addAttribute("projects", liveProjects);
 		model.addAttribute("projectDocuments", liveProjectDouments);
 		model.addAttribute("selectedProject", liveProject);
+		return "document-main";
+	}
+
+	@RequestMapping(value = "/stage", method = RequestMethod.GET)
+	public String showDocumentsForStage(@RequestParam(value = "pid") int pid,
+			@RequestParam(value = "sid") int sid, Model model) {
+		LiveProject liveProject = LiveProject.findLiveProject(pid);
+		List<LiveProject> liveProjects = LiveProject.findAllLiveProjects();
+		LiveProjectStage stage = LiveProjectStage.findLiveProjectStage(sid);
+		Set<LiveProjectStageDocument> stageDocuments = stage
+				.getLiveProjectStageDocuments();
+
+		model.addAttribute("projects", liveProjects);
+		model.addAttribute("selectedProject", liveProject);
+		model.addAttribute("selectedStage", stage);
+		model.addAttribute("stageDocuments", stageDocuments);
 		return "document-main";
 	}
 }
